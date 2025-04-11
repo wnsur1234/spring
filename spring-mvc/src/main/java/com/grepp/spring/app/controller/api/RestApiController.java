9b4b2f6cb07c502ebb3932c6f1f7a1700e0a0f2f
azi-prac <azimemory@gmail.com>
2025-04-11 09:51:16 +0900

    lec : day 29 1

--- spring-mvc/src/main/java/com/grepp/spring/app/controller/api/RestApiController.java
+++ spring-mvc/src/main/java/com/grepp/spring/app/controller/api/RestApiController.java
@@ -2,14 +2,15 @@ package com.grepp.spring.app.controller.api;
 
 import com.grepp.spring.app.controller.api.form.RestForm;
 import com.grepp.spring.app.controller.api.payload.RestPayload;
-import java.time.LocalDateTime;
+import com.grepp.spring.infra.response.ApiResponse;
 import java.time.OffsetDateTime;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,4 +26,42 @@ public class RestApiController {
         OffsetDateTime now = OffsetDateTime.now();
         return new RestPayload(1, "aaa@aaa.com", now, now.toEpochSecond());
     }
+    
+    @GetMapping
+    public ResponseEntity<ApiResponse<RestPayload>> get(){
+        OffsetDateTime now = OffsetDateTime.now();
+        RestPayload restPayload = new RestPayload(1, "aaa@aaa.com", now, now.toEpochSecond());
+        return ResponseEntity.ok(ApiResponse.success(restPayload));
+    }
+    
+    @DeleteMapping
+    public ResponseEntity<ApiResponse<RestPayload>> delete(){
+        OffsetDateTime now = OffsetDateTime.now();
+        return ResponseEntity.ok(ApiResponse.noContent());
+    }
+    
+    @PostMapping
+    public ResponseEntity<ApiResponse<RestPayload>> post(
+        RestPayload restPayload
+    ){
+        OffsetDateTime now = OffsetDateTime.now();
+        return ResponseEntity.ok(ApiResponse.noContent());
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
