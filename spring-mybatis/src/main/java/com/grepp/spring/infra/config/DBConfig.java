ab133005bcefe47fa05dc989e078a0dc482b7663
azi-prac <azimemory@gmail.com>
2025-04-14 16:13:01 +0900

    lec : day 30 6

--- spring-mybatis/src/main/java/com/grepp/spring/infra/config/DBConfig.java
+++ spring-mybatis/src/main/java/com/grepp/spring/infra/config/DBConfig.java
@@ -8,13 +8,17 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@Order(1)
+@EnableTransactionManagement
 public class DBConfig {
     
     @Value("${spring.datasource.url}")
