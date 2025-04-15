afd7b8dc03975a3dc3d0aa011d62f5a444d6000c
azi-prac <azimemory@gmail.com>
2025-04-15 17:17:59 +0900

    lec : day 31 6

--- spring-mybatis/src/main/webapp/assets/js/book-list.js
+++ spring-mybatis/src/main/webapp/assets/js/book-list.js
@@ -3,7 +3,8 @@ const size = 3;
 const observeTarget = document.querySelector(".target");
 
 const createBook = data => {
-  const clone = document.querySelector('#bookListTemplate').firstElementChild.cloneNode(true);
+  const clone = document.querySelector(
+      '#bookListTemplate').firstElementChild.cloneNode(true);
   const thumbnail = clone.querySelector('.thumbnail');
   const title = clone.querySelector('.title');
   const author = clone.querySelector('.author');
@@ -14,30 +15,32 @@ const createBook = data => {
   return clone;
 }
 
-document.addEventListener('scroll', ev => {
-  observeTarget.style.display='block';
+document.addEventListener('dblclick', ev => {
+  observeTarget.style.display = 'block';
 });
 
-document.addEventListener('scrollend', ev => {
-  observeTarget.style.display='none';
+document.addEventListener('scroll', ev => {
+  observeTarget.style.display = 'block';
 });
 
 const intersectionObserver = new IntersectionObserver(
     async function (entries) {
 
-  const response = await fetch(`http://localhost:8080/api/book/list?page=${++page}&size=${size}`);
-  const data = await response.json();
-  const books = data.data.bookInfos;
+      const response = await fetch(
+          `http://localhost:8080/api/book/list?page=${++page}&size=${size}`);
+      const data = await response.json();
+      const books = data.data.bookInfos;
 
-  if(!books || books.length === 0){
-    intersectionObserver.unobserve(observeTarget);
-    return;
-  }
+      if (!books || books.length === 0) {
+        intersectionObserver.unobserve(observeTarget);
+        return;
+      }
 
-  books.forEach(e => {
-      const clone = createBook(e);
-      document.querySelector('.book-list').appendChild(clone);
-  })
-});
+      books.forEach(e => {
+        const clone = createBook(e);
+        document.querySelector('.book-list').appendChild(clone);
+      })
+      observeTarget.style.display = 'none';
+    });
 
 intersectionObserver.observe(observeTarget);
