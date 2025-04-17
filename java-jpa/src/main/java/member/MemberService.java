05fff82a40db7d39bd6a71965f025b0fc6cc07c7
azi-prac <azimemory@gmail.com>
2025-04-17 16:03:39 +0900

    lec: day 32 6

--- java-jpa/src/main/java/member/MemberService.java
+++ java-jpa/src/main/java/member/MemberService.java
@@ -2,6 +2,8 @@ package member;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import java.time.LocalDateTime;
+import java.util.List;
 import jpa.JpaTemplate;
 
 public class MemberService {
@@ -19,6 +21,18 @@ public class MemberService {
         return member;
     }
     
+    public List<Member> findAll(){
+        try(EntityManager em = jpaTemplate.createEntityManager()){
+            return memberRepository.findAll(em);
+        }
+    }
+    
+    public List<Member> findWithCreatedAt(LocalDateTime start, LocalDateTime end){
+        try(EntityManager em = jpaTemplate.createEntityManager()){
+            return memberRepository.findWithCreatedAt(em, start, end);
+        }
+    }
+    
     public void add(Member member){
         
         EntityManager em = jpaTemplate.createEntityManager();
@@ -33,6 +47,76 @@ public class MemberService {
         }finally {
             em.close();
         }
-        
     }
+    
+    void modify(String userId, String password){
+        EntityManager em = jpaTemplate.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        
+        try{
+            tx.begin();
+            Member member = em.find(Member.class, userId);
+            member.modifyPassword(password);
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+    }
+    
+    void merge(Member member){
+        EntityManager em = jpaTemplate.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try{
+            tx.begin();
+            // 엔터티 매니저가 관리 중인 지 여부 확인
+            System.out.println("before merge : " +  em.contains(member));
+            Member result = em.merge(member);
+            tx.commit();
+            System.out.println("after merge : " +  em.contains(result));
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+    }
+    
+    void detach(String userId, String password){
+        EntityManager em = jpaTemplate.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        
+        try{
+            tx.begin();
+            // 영속 상태
+            Member member = em.find(Member.class, userId);
+            // 준영속 상태
+            em.detach(member);
+            member.modifyPassword(password);
+            
+            System.out.println("after detach : " + em.contains(member));
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+    }
+    
+    public void remove(Member member){
+        EntityManager em = jpaTemplate.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        
+        try{
+            tx.begin();
+            member = em.find(Member.class, member.getUserId());
+            em.remove(member);
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+    }
+    
 }
--- java-jpa/src/test/java/member/MemberServiceTest.java
+++ java-jpa/src/test/java/member/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package member;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 class MemberServiceTest {
 
@@ -13,11 +14,48 @@ class MemberServiceTest {
     @Test
     void add(){
         Member member = new Member();
-        member.setUserId("grepp3");
-        member.setPassword("1234");
+        member.setUserId("grepp");
+        member.setPassword("123456");
         member.setTel("01022223333");
         member.setEmail("aaa@ppp.jjj");
         memberService.add(member);
+    }
+    
+    @Test
+    void modify(){
+        memberService.modify("grepp", "8888888");
+    }
+    
+    @Test
+    void merge(){
+        Member member = new Member();
+        member.setUserId("grepp010101010");
+        memberService.merge(member);
+    }
+    
+    @Test
+    void detach(){
+        memberService.detach("grepp", "*****");
+    }
+    
+    @Test
+    void remove(){
+        Member member = new Member();
+        member.setUserId("grepp");
+        memberService.remove(member);
+    }
+    
+    @Test
+    void findAll(){
+        memberService.findAll().forEach(System.out::println);
+    }
+    
+    @Test
+    void findWithCreatedAt(){
+        LocalDateTime start = LocalDateTime.of(2023,1,1,1,1,1);
+        LocalDateTime end = LocalDateTime.of(2024,1,1,1,1,1);
+        memberService.findWithCreatedAt(start, end)
+            .forEach(System.out::println);
     }
 
 }
