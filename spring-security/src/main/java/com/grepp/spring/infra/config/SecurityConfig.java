c0e5908d5cc90175110eecffe8b8a01c0a2261c9
azi-prac <azimemory@gmail.com>
2025-04-17 16:44:20 +0900

    lec: day 32

--- spring-security/src/main/java/com/grepp/spring/infra/config/SecurityConfig.java
+++ spring-security/src/main/java/com/grepp/spring/infra/config/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig {
                 (requests) -> requests
                                   .requestMatchers(GET, "/", "/assets/**", "/download/**").permitAll()
                                   .requestMatchers(GET, "/book/list").permitAll()
+                                  .requestMatchers(GET, "/api/book/list").permitAll()
                                   .requestMatchers(GET, "/api/member/exists/*").permitAll()
                                   .requestMatchers(GET, "/member/signup").permitAll()
                                   .requestMatchers(GET, "/member/signin").permitAll()
--- spring-security/src/main/webapp/WEB-INF/view/error/redirect.jsp
+++ spring-security/src/main/webapp/WEB-INF/view/error/redirect.jsp
@@ -1,5 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@include file="/WEB-INF/view/include/page.jsp" %>
+
 <html lang='ko'>
 <head>
     <title>Title</title>
